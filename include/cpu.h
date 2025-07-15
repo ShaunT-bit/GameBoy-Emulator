daf4285fb92bb018d0483adfe5383a40a9aa0c92
ShaunT-bit <tshaun114@gmail.com>
2025-07-15 19:28:43 +0400

    fixed the cpu

--- include/cpu.h
+++ include/cpu.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "common.h"
-#include "instructions.h"
+#include <common.h>
+#include <instructions.h>
 
 typedef struct {
     u8 a;
@@ -16,10 +16,10 @@ typedef struct {
     u16 sp;
 } cpu_registers;
 
-
 typedef struct {
     cpu_registers regs;
 
+    //current fetch...
     u16 fetched_data;
     u16 mem_dest;
     bool dest_is_mem;
@@ -27,8 +27,11 @@ typedef struct {
     instruction *cur_inst;
 
     bool halted;
+    bool stepping;
+
     bool int_master_enabled;
     u8 ie_register;
+
 } cpu_context;
 
 cpu_registers *cpu_get_regs();
@@ -36,12 +39,12 @@ cpu_registers *cpu_get_regs();
 void cpu_init();
 bool cpu_step();
 
-typedef void (*IN_PROC) (cpu_context *);
+typedef void (*IN_PROC)(cpu_context *);
 
-IN_PROC intst_get_processor(in_type type);
+IN_PROC inst_get_processor(in_type type);
 
-#define CPU_FLAG_Z BIT(ctx -> regs.f, 7)
-#define CPU_FLAG_C BIT(ctx -> regs.f, 4)
+#define CPU_FLAG_Z BIT(ctx->regs.f, 7)
+#define CPU_FLAG_C BIT(ctx->regs.f, 4)
 
 u16 cpu_read_reg(reg_type rt);
 void cpu_set_reg(reg_type rt, u16 val);
